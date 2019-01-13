ac05621812dd9c3d864c0a15012338de9e3b8d8f
nikrb <nik_rb@yahoo.com>
2019-01-13 00:59:41 +0000

    initial commit

---
+++ replaceVars.js
@@ -0,0 +1,69 @@
+/*
+we transform the var declarations to const or let in order to simplify
+catching the variable reassignents, which require a let declaration
+
+1. for loops - easy, just change var to let
+2. transform multiline declarations (separated with commas) giving each
+    declaration it's own var line declaration
+3. swap all remaining var's to const's
+4. check through for const reassignement and change declaration to let
+ */
+/* eslint-disable no-console */
+const fs = require('fs');
+const { exec } = require('child_process');
+const globby = require('globby');
+require('dotenv').config();
+
+const {
+  const2let,
+  fors,
+  separateMultiLineVars,
+  varconst
+} = require('./replacers');
+
+let filelist;
+if (process.argv.length > 2) {
+  filelist = Promise.resolve([process.argv[2]]);
+} else if (process.env.FCC_BASE_DIR) {
+  console.log('directory:',
+    `${process.env.FCC_BASE_DIR}/curriculum/challenges/english/**/*.md`);
+  filelist = globby([
+    /* eslint-disable max-len */
+    // `${process.env.FCC_BASE_DIR}/curriculum/challenges/english/02-javascript-algorithms-and-data-structures`,
+    `${process.env.FCC_BASE_DIR}/curriculum/challenges/english/**/*.md`,
+    `!${process.env.FCC_BASE_DIR}/curriculum/challenges/english/01-responsive-web-design`,
+    `!${process.env.FCC_BASE_DIR}/curriculum/challenges/english/02-javascript-algorithms-and-data-structures/basic-javascript`,
+    `!${process.env.FCC_BASE_DIR}/curriculum/challenges/english/02-javascript-algorithms-and-data-structures/es6`,
+    /* eslint-enable max-len */
+  ]);
+}
+if (filelist) {
+  filelist.then(paths => {
+    paths.forEach(filepath => {
+      let str;
+      console.log('processing:', filepath);
+      try {
+        str = fs.readFileSync(filepath, 'utf8');
+      } catch (e) {
+        console.error('file read failed [${filepath}]', e);
+      }
+      const res = const2let(varconst(separateMultiLineVars(fors(str))));
+      // console.log('results:', res);
+      try {
+        if (res !== str) {
+          const opfile = filepath.replace(/\.md/, '.out.md');
+          const difile = filepath.replace(/\.md/, '.out.md.dif');
+          console.log('out file:', opfile);
+          fs.writeFileSync(opfile, res);
+          exec( `diff ${filepath} ${opfile} > ${difile}`);
+        }
+      } catch (e) {
+        console.error(`file write failed [${filepath}]`);
+      }
+    });
+  })
+  .catch(err => console.error(err));
+} else {
+  console.error('Failed to find files');
+}
+
---
+++ replacers.js
@@ -0,0 +1,64 @@
+function getIndent(s) {
+  const count = s.search(/\w/);
+  if (count < 1) { return ''; }
+  return ' '.repeat(count - 1);
+}
+
+const forre = /(for\s*\()\s*\bvar\b\s+([a-zA-Z0-9]+)/g;
+exports.fors = str => {
+  return str.replace(forre, '$1let $2');
+};
+
+const varre = /var\s+(\w+)/g;
+exports.varconst = str => {
+  return str.replace(varre, 'const $1');
+};
+
+const constre = /const\s+(\w+)\s+=/g;
+exports.const2let = str => {
+  const sections = str.split('</section>');
+  return sections.reduce((acc, cur) => {
+    const closingSection = cur.includes('<section') ? '</section>' : '';
+    return acc.concat(
+      cur.replace(constre, match => {
+        const varname = match.split(' ')[1];
+        // template string fubar's editor
+        const varass = new RegExp(
+          '(const)?\\s+' + varname + '\\s+[+\\-\\/*]?=\\s*[\\w]+', 'g');
+        const reass = cur.match(varass);
+        // use let if we have a reassignment
+        if (reass !== null) {
+          const constcount = reass.reduce(
+            (a, c) => a + c.includes('const'), 0);
+          if (reass.length > 1 && constcount < 2 ) {
+            return `let ${varname} =`;
+          }
+        }
+        return match;
+      }), closingSection);
+  }, '');
+};
+
+const separateMultilineVarsRE = /\s*var\s+[\s\w=,.]+;/g;
+exports.separateMultiLineVars = str => {
+  return str.replace(separateMultilineVarsRE, match => {
+    const indent = getIndent(match);
+    const bits = match.split(',');
+    if (bits.length > 1) {
+      const separateLines = bits.reduce((acc, cur, ndx, arr) => {
+        const c = cur.trim();
+        // last line will already have semicolon
+        let ret = acc.concat(`\n${indent}var ${c}`);
+        if (ndx === 0) {
+          // first line will have var
+          ret = acc.concat(`\n${indent}${c};`);
+        } else if (ndx < arr.length - 1) {
+          ret = acc.concat(`\n${indent}var ${c};`);
+        }
+        return ret;
+      }, '');
+      return separateLines;
+    }
+    return match;
+  });
+};
